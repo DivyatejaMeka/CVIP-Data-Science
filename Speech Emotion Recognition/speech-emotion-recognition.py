bf5b99b6df08bb03b3074788abbc3b9bb6138525
DivyatejaMeka <140238723+DivyatejaMeka@users.noreply.github.com>
2023-10-23 08:53:07 +0530

    Add files via upload

---
+++ Speech Emotion Recognition/speech-emotion-recognition.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+#import os
+#for dirname, _, filenames in os.walk('/kaggle/input'):
+#    for filename in filenames:
+#        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+
+# ## Import Libraries
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+from IPython.display import Audio
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# ## Loading Dataset
+
+# In[3]:
+
+
+paths = []
+labels = []
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        paths.append(os.path.join(dirname, filename))
+        label = filename.split('_')[-1]
+        label = label.split('.')[0]
+        labels.append(label.lower())
+print('Dataset is Loaded')
+
+
+# In[4]:
+
+
+paths[:5]
+
+
+# In[5]:
+
+
+labels[:5]
+
+
+# In[6]:
+
+
+df = pd.DataFrame()
+df['speech'] = paths
+df['label'] = labels
+df.head()
+
+
+# In[7]:
+
+
+df['label'].value_counts()
+
+
+# # EDA
+
+# In[8]:
+
+
+def waveplot(data, sr, emotion):
+    plt.figure(figsize=(10,4))
+    plt.title(emotion, size=20)
+    librosa.display.waveshow(data, sr=sr)
+    plt.show()
+    
+def spectogram(data, sr, emotion):
+    x = librosa.stft(data)
+    xdb = librosa.amplitude_to_db(abs(x))
+    plt.figure(figsize=(11,4))
+    plt.title(emotion, size=20)
+    librosa.display.specshow(xdb, sr=sr, x_axis='time', y_axis='hz')
+    plt.colorbar()
+
+
+# In[9]:
+
+
+emotion = 'fear'
+path = np.array(df['speech'][df['label']==emotion])[0]
+data, sampling_rate = librosa.load(path)
+waveplot(data, sampling_rate, emotion)
+spectogram(data, sampling_rate, emotion)
+Audio(path)
+
+
+# In[10]:
+
+
+emotion = 'angry'
+path = np.array(df['speech'][df['label']==emotion])[1]
+data, sampling_rate = librosa.load(path)
+waveplot(data, sampling_rate, emotion)
+spectogram(data, sampling_rate, emotion)
+Audio(path)
+
+
+# In[11]:
+
+
+emotion = 'disgust'
+path = np.array(df['speech'][df['label']==emotion])[0]
+data, sampling_rate = librosa.load(path)
+waveplot(data, sampling_rate, emotion)
+spectogram(data, sampling_rate, emotion)
+Audio(path)
+
+
+# In[12]:
+
+
+emotion = 'neutral'
+path = np.array(df['speech'][df['label']==emotion])[0]
+data, sampling_rate = librosa.load(path)
+waveplot(data, sampling_rate, emotion)
+spectogram(data, sampling_rate, emotion)
+Audio(path)
+
+
+# In[13]:
+
+
+emotion = 'sad'
+path = np.array(df['speech'][df['label']==emotion])[0]
+data, sampling_rate = librosa.load(path)
+waveplot(data, sampling_rate, emotion)
+spectogram(data, sampling_rate, emotion)
+Audio(path)
+
+
+# In[14]:
+
+
+emotion = 'ps'
+path = np.array(df['speech'][df['label']==emotion])[0]
+data, sampling_rate = librosa.load(path)
+waveplot(data, sampling_rate, emotion)
+spectogram(data, sampling_rate, emotion)
+Audio(path)
+
+
+# In[15]:
+
+
+emotion = 'happy'
+path = np.array(df['speech'][df['label']==emotion])[0]
+data, sampling_rate = librosa.load(path)
+waveplot(data, sampling_rate, emotion)
+spectogram(data, sampling_rate, emotion)
+Audio(path)
+
+
+# # Feature Extraction
+
+# In[16]:
+
+
+def extract_mfcc(filename):
+    y, sr = librosa.load(filename, duration=3, offset=0.5)
+    mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
+    return mfcc
+
+
+# In[17]:
+
+
+extract_mfcc(df['speech'][0])
+
+
+# In[18]:
+
+
+X_mfcc = df['speech'].apply(lambda x: extract_mfcc(x))
+
+
+# In[19]:
+
+
+X_mfcc
+
+
+# In[20]:
+
+
+X = [x for x in X_mfcc]
+X = np.array(X)
+X.shape
+
+
+# In[21]:
+
+
+X = np.expand_dims(X, -1)
+X.shape
+
+
+# In[22]:
+
+
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder()
+y = enc.fit_transform(df[['label']])
+
+
+# In[23]:
+
+
+y = y.toarray()
+
+
+# In[24]:
+
+
+y.shape
+
+
+# # LSTM Model
+
+# In[25]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+
+model = Sequential([
+    LSTM(256, return_sequences=False, input_shape=(40,1)),
+    Dropout(0.2),
+    Dense(128, activation='relu'),
+    Dropout(0.2),
+    Dense(64, activation='relu'),
+    Dropout(0.2),
+    Dense(7, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+
+# In[26]:
+
+
+history = model.fit(X, y, validation_split=0.2, epochs=50, batch_size=64)
+
+
+# # Plotting Results
+
+# In[27]:
+
+
+epochs = list(range(50))
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, label='train accuracy')
+plt.plot(epochs, val_acc, label='val accuracy')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+
+
+# In[29]:
+
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.plot(epochs, loss, label='train loss')
+plt.plot(epochs, val_loss, label='val loss')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+
+# 
